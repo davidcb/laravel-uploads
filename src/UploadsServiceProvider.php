1d06ab2fb6884c1202401f2ba84c269e8fad2091
David Couto <davidcouto@gmail.com>
2019-02-21 13:22:55 +0000

    Added assets

--- src/UploadsServiceProvider.php
+++ src/UploadsServiceProvider.php
@@ -12,8 +12,11 @@ class UploadsServiceProvider extends ServiceProvider {
 			__DIR__ . '/config/crop.php' => config_path('crop.php'),
 			__DIR__ . '/config/upload.php' => config_path('upload.php'),
 		]);
+		$this->publishes([
+			__DIR__.'/assets' => public_path('vendor/laravel-uploads'),
+		], 'public');
 		$this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-		$this->loadViewsFrom(__DIR__ . '/resources/views', 'uploads');
+		$this->loadViewsFrom(__DIR__ . '/resources/views', 'laravel-uploads');
 		$this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 	}
 
